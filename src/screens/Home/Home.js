a5d109d528d325f0d83282d18431b19cf0d6e08a
Apple <apple@Apples-MacBook-Pro.local>
2023-04-13 15:56:33 +0530

    styling properties

--- src/screens/Home/Home.js
+++ src/screens/Home/Home.js
@@ -1,20 +1,33 @@
 import React from 'react';
-import { FlatList, Text } from 'react-native';
+import { FlatList, Text, View } from 'react-native';
 import { HomeStyles as styles } from './Home.style';
 
 const Home = () => {
   const renderItem = ({item, index}) => {
     return (
-      <Text style={[styles.textItem, styles.textColor]}>
-        {`Item No. ${index + 1}`}
-      </Text>
+			<View style={styles.listInnerContainer}>
+				<Text style={[styles.textItem, styles.textColor]}>
+					{`Item No. ${index + 1}`}
+				</Text>
+			</View>
     )
   }
   return (
-    <FlatList
-      data={[1, 2, 3, 4]}
-      renderItem={renderItem}
-    />
+    <View style={styles.container}>
+			<View style={styles.listContainer}>
+				<FlatList
+					data={[1, 2, 3, 4, 1, 2, 3, 4, 1, 2, 3, 4, 1, 2, 3, 4, 1, 2, 3, 4]}
+					renderItem={renderItem}
+				/>
+			</View>
+			<View style={styles.contentContainer}>
+				<View style={styles.floatingText}>
+					<Text>Floating text...</Text>
+				</View>
+				<Text style={styles.textItem}>1</Text>
+				<Text style={styles.textItem}>2</Text>
+			</View>
+    </View>
   )
 }
 
--- src/screens/Home/Home.style.js
+++ src/screens/Home/Home.style.js
@@ -1,13 +1,47 @@
 import { StyleSheet } from "react-native";
 import Colors from "../../utils/Colors";
 const HomeStyles = StyleSheet.create({
+	container: {
+		flex: 1,
+		flexDirection: 'column',
+		borderWidth: 5,
+		borderColor: 'red',
+	},
+	listContainer: {
+		flex: 0.5,
+		borderWidth: 5,
+		borderColor: 'green',
+	},
+	listInnerContainer: {
+		minHeight: 40,
+		// borderWidth: 1,
+	},
+	contentContainer: {
+		flex: 0.5,
+		borderWidth: 5,
+		borderColor: 'blue',
+		flexDirection: 'row',
+		alignItems: 'center',
+		justifyContent: 'center',
+		position: 'relative',
+	},
 	textItem: {
 		fontSize: 21,
 		textAlign: 'center',
 	},
 	textColor: {
 		color: Colors.secondary,
+	},
+	floatingText: {
+		position: 'absolute',
+		top: '45%',
+		right: 0,
 	}
 })
 
 export { HomeStyles};
+/*
+Flex box - 
+
+
+*/
