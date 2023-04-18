0c750852392374fcd67720b1c31e1afe94e63ea5
Apple <apple@Apples-MacBook-Pro.local>
2023-04-18 18:00:38 +0530

    Integration of redux persist

--- App.js
+++ App.js
@@ -4,7 +4,8 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import NavigationKeys from './src/navigation/keys';
 import Tabs from './src/navigation/TabNavigator';
 import { Provider } from 'react-redux';
-import { store } from './src/store/configureStore';
+import { persistor, store } from './src/store/configureStore';
+import { PersistGate } from 'redux-persist/integration/react';
 // import Profile from './src/screens/Profile/Profile.class';
 
 const Stack = createNativeStackNavigator();
@@ -12,15 +13,17 @@ const Stack = createNativeStackNavigator();
 const App = () => {
   return (
     <Provider store={store}>
-      <NavigationContainer>
-        <Stack.Navigator
-          screenOptions={{
-            headerShown: false,
-          }}
-        >
-          <Stack.Screen name={NavigationKeys.tabs} component={Tabs} />
-        </Stack.Navigator>
-      </NavigationContainer>
+      <PersistGate loading={null} persistor={persistor}>
+        <NavigationContainer>
+          <Stack.Navigator
+            screenOptions={{
+              headerShown: false,
+            }}
+          >
+            <Stack.Screen name={NavigationKeys.tabs} component={Tabs} />
+          </Stack.Navigator>
+        </NavigationContainer>
+      </PersistGate>
     </Provider>
   )
 }
--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,8 +1,43 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
+import { persistStore, persistReducer } from "redux-persist";
 import counterSlice from "../reducers/counterReducer";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import autoMergeLevel2 from 'redux-persist/lib/stateReconciler/autoMergeLevel2'
+// For ios - need to do pod-install after the installation of AsyncStorage library
+
+/*
+Persist Config -
+  interface PersistConfig<S, RS = any, HSS = any, ESS = any> {
+    version?: number;
+    storage: Storage;
+    key: string;
+    blacklist?: Array<string>;
+    whitelist?: Array<string>;
+    transforms?: Array<Transform<HSS, ESS, S, RS>>;
+    throttle?: number;
+    migrate?: PersistMigrate;
+    stateReconciler?: false | StateReconciler<S>;
+    getStoredState?: (config: PersistConfig<S, RS, HSS, ESS>) => Promise<PersistedState>;
+    debug?: boolean;
+    serialize?: boolean;
+    timeout?: number;
+    writeFailHandler?: (err: Error) => void;
+  }
+*/
+const persistConfig = {
+    key: 'root',
+    storage: AsyncStorage,
+    stateReconciler: autoMergeLevel2,
+}
+
+const rootReducer = combineReducers({
+    counter: counterSlice,
+})
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 export const store = configureStore({
-    reducer: {
-        counter: counterSlice,
-    }
+    reducer: persistedReducer
 })
+
+export const persistor = persistStore(store);
